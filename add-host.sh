870314732056f944224f59be934acadf9881320d
tryoo127 <tryoo127@gmail.com>
2022-07-22 01:13:21 +0800

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -17,27 +17,21 @@ exit 0
 fi
 clear
 
-Tukar Domain?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) make install; break;;
-        No ) exit;;
-    esac
-done
-clear
-
-echo -e "Masukkan Domain"
-read -p "Hostname / Domain: " host
-rm -f /var/lib/crot-script/ipvps.conf
+echo -e "MASUKKAN DOMAIN BARU ATAU TEKAN CTL C UTK EXIT"
+echo -e ""
+read -p "DOMAIN/SUBDOMAIN: " host
 rm -f /var/lib/premium-script/ipvps.conf
 rm -f /etc/v2ray/domain
-clear
 mkdir /etc/v2ray
 mkdir /var/lib/premium-script;
-mkdir /var/lib/crot-script;
-clear
-echo -e "Masukkan Domain Sekali Lagi"
-read -p "Hostname / Domain: " host
-echo "IP=$host" >> /var/lib/crot-script/ipvps.conf
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 echo "$host" >> /etc/v2ray/domain
+clear
+sleep 1
+clear
+echo -e "============================================="
+echo -e " PERTUKARAN DOMAIN BERJAYA "
+echo -e "============================================="
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
