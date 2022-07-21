c9f844db0d59920a358ecbccadfa7cf26c969cf9
tryoo127 <tryoo127@gmail.com>
2022-07-21 22:10:18 +0800

    Add files via upload

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -7,16 +7,10 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 IZIN=$( curl -sS https://raw.githubusercontent.com/tryoo127/access/main/ip | awk '{print $4}' | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 clear
-echo -e "${green}TAHNIAH! ANDA DIBENARKAN MENGGUNAKAN SCRIPT @mexxv! ${NC}"
-sleep 5
-else
-clear
-echo ""
 rm -f setup.sh
-echo '                            ...                           '
-echo -e "${red}MAAF! ANDA TIDAK DIBENARKAN MENGGUNAKAN SCRIPT INI! ${NC}"
+echo -e "${green}MAAF! ANDA TIDAK DIBENARKAN MENGGUNAKAN SCRIPT INI! ${NC}"
 echo '                            ...                           ' 
-sleep 20
+sleep 7
 exit 0
 fi
 clear
