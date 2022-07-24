444c57c39fc502fb2e941f890425d40a09cef2fb
tryoo127 <tryoo127@gmail.com>
2022-07-25 06:36:53 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 IZIN=$( curl -sS https://raw.githubusercontent.com/tryoo127/access/main/ip | awk '{print $4}' | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 clear
-echo -e "${green}          PERMISSION ACCEPTED!${NC}"
+echo -e "${green}            PERMISSION ACCEPTED!${NC}"
 else
 clear
 echo ""
