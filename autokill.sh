15c6541c402fad415210a2aa92e1d4adb7ccb908
tryoo127 <tryoo127@gmail.com>
2022-08-01 10:26:49 +0800

    Update autokill.sh

--- autokill.sh
+++ autokill.sh
@@ -16,27 +16,6 @@ sleep 7
 exit 0
 fi
 clear
-
-green='\e[0;32m'
-red='\e[31m'
-NC='\e[0m'
-MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl -sS https://raw.githubusercontent.com/xoolvpn/access/main/ip | awk '{print $4}' | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-clear
-echo -e "${green}TAHNIAH! ANDA DIBENARKAN MENGGUNAKAN SCRIPT @XoolVPN! ${NC}"
-sleep 5
-else
-clear
-echo ""
-rm -f setup.sh
-echo '                            ...                           '
-echo -e "${red}MAAF! ANDA TIDAK DIBENARKAN MENGGUNAKAN SCRIPT INI! ${NC}"
-echo '                            ...                           ' 
-sleep 20
-exit 0
-fi
-clear
 echo -e ""
 echo -e "======================================"
 echo -e ""
