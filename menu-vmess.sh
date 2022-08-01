8ac8ca089553e8b7a47bdcab0fac7b168ed9bc05
tryoo127 <tryoo127@gmail.com>
2022-08-01 17:28:31 +0800

    Update menu-vmess.sh

--- menu-vmess.sh
+++ menu-vmess.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
-
+red='\e[1;31m'
+green='\e[0;32m'
+cyan='\e[0;36m'
+NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 IZIN=$( curl -sS https://raw.githubusercontent.com/tryoo127/access/main/ip | awk '{print $4}' | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 clear
-echo -e "${green}   TAHNIAH! ANDA DIBENARKAN MENGGUNAKAN SCRIPT INI...${NC}"
+echo -e "${green}            PERMISSION ACCEPTED!${NC}"
 else
 clear
 echo ""
 rm -f setup.sh
-echo '                            ...                           '
-echo -e "${red}        MAAF! ANDA TIDAK DIBENARKAN MENGGUNAKAN SCRIPT INI...${NC}"
-echo '                            ...                           ' 
+echo -e "${red}              PERMISSION DENIED!${NC}"
+echo -e "${green} PLEASE CONTACT @XoolVPN TO REGISTER YOUR I.P${NC}"
 sleep 7
 exit 0
 fi
 clear
 
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m                      V2RAY VLESS MENU                        \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[0;37m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;37;46m           ◎ V2RAY VMESS MENU ◎                    \e[0m"
+echo -e
+echo -e " [1]\e[0m•\e[1;36mCREATE NEW USER\e[0m"
+echo -e " [2]\e[0m•\e[1;36mRENEW USER ACCOUNT\e[0m"
+echo -e " [3]\e[0m•\e[1;36mDELETE USER ACCOUNT\e[0m"
+echo -e " [4]\e[0m•\e[1;36mCHECK USER LOGIN\e[0m"
+echo -e
+echo -e "\E[0;37;46m           ◎ Moded By 𝑿𝒐𝒐𝒍𝚅𝙿𝙽 ◎                    \e[0m"
+echo -e "\033[0;37m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-echo -e "\033[0;34m    * [1]\e[0m: Create V2RAY Vmess Websocket Account\e[0m"
-echo -e "\033[0;34m    * [2]\e[0m: Deleting V2RAY Vmess Websocket Account\e[0m"
-echo -e "\033[0;34m    * [3]\e[0m: Extending Vmess Account Active Life\e[0m"
-echo -e "\033[0;34m    * [4]\e[0m: Check User Login V2RAY\e[0m"
-echo -e ""
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-read -p "        Select From Options [1-5 or x]: " menuvmess
+read -p "        Select Options From [1-4 or x]: " menuvmess
 echo -e ""
 case $menuvmess in
 1)
 add-ws
 ;;
 2)
-del-ws
+renew-ws
 ;;
 3)
-renew-ws
+del-ws
 ;;
 4)
 cek-ws
